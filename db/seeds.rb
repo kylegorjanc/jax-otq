edc354d3c22a478223c032cb8c072b55e2ada3fd
Kyle Gorjanc <cinnarunner@gmail.com>
2015-11-05 15:44:48 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@
 
 
 runners = Runner.create([
-	{first_name: "Becki", 	last_name: "Spellman", 		age: 32, profession: "", website: "", has_coach: true, coach_name: "Glenn Andrews", twitter_handle: "@beckimichael9", has_spouse: true, has_children: false, num_of_children: 0, is_qualified: false, q_standard: "", marathon_pr: 2015-10-18T02:44:44Z, first_marathon_time:2000-01-01T00:00:00Z, avg_job_hrs: nil, bio: "'Rocket' on Salty Running", peak_mileage_wkly: 80 },
-	{first_name: "Jessica", last_name: "Kuhr Odorcic", 	age: 34, profession: "", website: "", has_coach: true, coach_name: "Glenn Andrews", twitter_handle: "jessicaodorcic", has_children: false, num_of_children: 0, is_qualified: true, q_standard: "B", marathon_pr: 2012-04-22T02:51:50Z, first_marathon_time:2012-04-22T02:51:50Z, avg_job_hrs: nil, bio: "Check out an interview with Jessica on Salty Running", peak_mileage_wkly: 80}
+	{first_name: "Becki", 	last_name: "Spellman", 		age: 32, profession: "", website: "", has_coach: true, coach_name: "Glenn Andrews", twitter_handle: "@beckimichael9", has_spouse: true, has_children: false, num_of_children: 0, is_qualified: false, q_standard: "", marathon_pr: "2:44:44", first_marathon_time:"", avg_job_hrs: nil, bio: "'Rocket' on Salty Running", peak_mileage_wkly: 80 },
+	{first_name: "Jessica", last_name: "Kuhr Odorcic", 	age: 34, profession: "", website: "", has_coach: true, coach_name: "Glenn Andrews", twitter_handle: "jessicaodorcic", has_children: false, num_of_children: 0, is_qualified: true, q_standard: "B", marathon_pr: "2:51:50", first_marathon_time:"", avg_job_hrs: nil, bio: "Check out an interview with Jessica on Salty Running", peak_mileage_wkly: 80}
 	])
 
 
